e6481ba77577a919a6dfd91e2a43242c900b8fd1
win10devoffshore <win10devoffshore@hotmail.com>
2018-02-01 12:33:32 -0800

    add server

--- server.js
+++ server.js
@@ -1,38 +1,22 @@
-var http = require('http');
-var fs = require("fs");
+// Modules
+const http = require('http');
+const fs = require('fs');
 
-http.createServer(function(request, response) {
+// Server configuration
+const hostname = '127.0.0.1';
+const port = 3000;
 
-	if(request.url === "/index"){
-		sendFileContent(response, "index.html", "text/html");
-	}
-	else if(request.url === "/"){
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
-	}
-	else if(/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
-	}
-	else if(/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())){
-		sendFileContent(response, request.url.toString().substring(1), "text/css");
-	}
-	else{
-		console.log("Requested URL is: " + request.url);
-		response.end();
-	}
-}).listen(3000);
-
-function sendFileContent(response, fileName, contentType){
-	fs.readFile(fileName, function(err, data){
-		if(err){
-			response.writeHead(404);
-			response.write("Not Found!");
-		}
-		else{
-			response.writeHead(200, {'Content-Type': contentType});
-			response.write(data);
-		}
-		response.end();
-	});
-}
-
+// Render page
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    const server = http.createServer(function (request, response) {
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(html);
+        response.end();
+    }).listen(port, hostname, () => {
+        // Console
+        console.log(`Server running at http://${hostname}:${port}/`);
+    });
+});
